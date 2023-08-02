fe9216517a134fc566c2f4f2f1d9a3c3dc10ec60
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2023-08-02 16:39:56 -0300

    translate to PT

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,84 @@
+const diceIcon = document.querySelector(".icon");
+diceIcon.addEventListener("click", generateAdvice);
+
 async function generateAdvice() {
 
-  const url = "https://api.adviceslip.com/advice";
-  const resultado = await fetch(url);
-  const adviceContent = await resultado.json();
+    clickAnimation();
 
-  const adviceId = adviceContent.slip.id;
-  const adviceDescription = adviceContent.slip.advice;
+    const url = "https://api.adviceslip.com/advice";
+    const resultado = await fetch(url);
+    const adviceContent = await resultado.json();
 
-  document.querySelector(".card .id").remove()
-  document.querySelector(".card .quote").remove()
+    const adviceId = adviceContent.slip.id;
+    const adviceDescription = adviceContent.slip.advice;
 
-  const $newId = document.createElement("p")
-  const $newQuote = document.createElement("strong")
+    const adviceDescriptionPT = await translate(adviceDescription);
 
-  $newId.innerHTML = `Advice #${adviceId}`
-  $newQuote.innerHTML = adviceDescription
+    document.querySelector(".card .id").remove();
+    document.querySelector(".card .quote").remove();
 
-  $newId.classList.add("id");
-  $newQuote.classList.add("quote")
+    const $newId = document.createElement("p");
+    const $newQuote = document.createElement("strong");
 
-  document.querySelector(".card").appendChild($newId)
-  document.querySelector(".card").appendChild($newQuote)
+    $newId.innerHTML = `Conselho #${adviceId}`;
+    $newQuote.innerHTML = adviceDescriptionPT;
 
+    $newId.classList.add("id");
+    $newQuote.classList.add("quote");
+
+    document.querySelector(".card").appendChild($newId);
+    document.querySelector(".card").appendChild($newQuote);
+
+    hideDividerBigQuote()
 }
 
-const diceIcon = document.querySelector(".icon");
+async function translate(value) {
+    const url = "https://text-translator2.p.rapidapi.com/translate";
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": "5bc6f4fdfdmsh4f2174a64203595p1c69a6jsn1054a151b5a0",
+        "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
+      },
+      body: new URLSearchParams({
+        source_language: "en",
+        target_language: "pt",
+        text: value,
+      }),
+    };
 
-diceIcon.addEventListener("click", generateAdvice);
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      const translatedText = result.data.translatedText;
+
+      return translatedText;
+
+    } catch (error) {
+      console.error(error);
+  }
+}
+
+function hideDividerBigQuote() {
+
+    const quote = document.querySelector(".quote")
+
+    if(quote.clientHeight > 240) {
+        quote.classList.add("big")
+    }
+}
+
+
+function clickAnimation() {
+
+    const dice = document.querySelector(".icon .dice");
+
+    if (dice.classList.contains("roll")) {
+      dice.classList.remove("roll");
+      dice.offsetWidth;
+      dice.classList.add("roll");
+    } else {
+      dice.classList.add("roll");
+    }
+}
