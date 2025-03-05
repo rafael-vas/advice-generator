5ddf0da2824b5095dd0e1e61c4e8b20d62caf6f4
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2025-03-05 12:13:55 -0300

    remove PT translation

--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,12 @@ async function generateAdvice() {
     clickAnimation();
 
     const url = "https://api.adviceslip.com/advice";
-    const resultado = await fetch(url);
-    const adviceContent = await resultado.json();
+    const result = await fetch(url);
+    const adviceContent = await result.json();
 
     const adviceId = adviceContent.slip.id;
     const adviceDescription = adviceContent.slip.advice;
 
-    const adviceDescriptionPT = await translate(adviceDescription);
 
     document.querySelector(".card .id").remove();
     document.querySelector(".card .quote").remove();
@@ -20,8 +19,8 @@ async function generateAdvice() {
     const $newId = document.createElement("p");
     const $newQuote = document.createElement("strong");
 
-    $newId.innerHTML = `Conselho #${adviceId}`;
-    $newQuote.innerHTML = adviceDescriptionPT;
+    $newId.innerHTML = `Advice #${adviceId}`;
+    $newQuote.innerHTML = adviceDescription;
 
     $newId.classList.add("id");
     $newQuote.classList.add("quote");
@@ -30,34 +29,6 @@ async function generateAdvice() {
     document.querySelector(".card").appendChild($newQuote);
 
     hideDividerBigQuote()
-}
-
-async function translate(value) {
-    const url = "https://text-translator2.p.rapidapi.com/translate";
-    const options = {
-      method: "POST",
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-        "X-RapidAPI-Key": "5bc6f4fdfdmsh4f2174a64203595p1c69a6jsn1054a151b5a0",
-        "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
-      },
-      body: new URLSearchParams({
-        source_language: "en",
-        target_language: "pt",
-        text: value,
-      }),
-    };
-
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      const translatedText = result.data.translatedText;
-
-      return translatedText;
-
-    } catch (error) {
-      console.error(error);
-  }
 }
 
 function hideDividerBigQuote() {
